2312cab8403a32a89320998c511a6dc453ce71f3
Harley Mcphee <harley.1011@gmail.com>
2013-05-14 23:15:47 -0600

    Pairing thread now sends the color of the player once a socket is accepted

--- src/com/hmml/knightswatch_server/PairingThread.java
+++ src/com/hmml/knightswatch_server/PairingThread.java
@@ -1,6 +1,7 @@
 package com.hmml.knightswatch_server;
 
 import java.io.IOException;
+import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -29,19 +30,23 @@ public class PairingThread extends Thread {
 			// wait for a client connection, socket is obtained when a client connects
 			Socket socket1 = null;
 			Socket socket2 = null;
+			PrintStream out = null;
 			try {
 				socket1 = server.accept();
 				System.out.println("Accepted connection 1: " + socket1.getInetAddress().toString());
-
+				out = new PrintStream(socket1.getOutputStream());
+				out.println("Black");
 				socket2 = server.accept();
 				System.out.println("Accepted connection 2: " + socket2.getInetAddress().toString());
+				out = new PrintStream(socket2.getOutputStream());
+				out.println("White");
 				
 				// first param is gameId, 
 				// we use the current position in the GameThreads array as the Id
 				// starts at 0
 				GameThread newGame = new GameThread(GameServer.GameThreads.size(), socket1, socket2);
 				GameServer.GameThreads.add(newGame);
-				newGame.run();
+				newGame.start();
 				
 			} catch (IOException e) {
 				e.printStackTrace();
